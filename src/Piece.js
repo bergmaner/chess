9a29a45fda1719f82f7a84dfe1991202762618dc
bergmaner <bergkacper@gmail.com>
2020-10-14 17:02:05 +0200

    dragging pieces

--- src/Piece.js
+++ src/Piece.js
@@ -1,23 +1,43 @@
 import React from "react";
+import { useDrag, DragPreviewImage } from "react-dnd";
 import styled from "styled-components";
 
 const PieceContainer = styled.div`
-cursor: grab;
-width: 100%;
-height: 100%;
-display: flex;
-justify-content: center;
-align-items: center;
-img{
+  cursor: grab;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${props => props.isDragging ? 0 :1};
+  img {
     height: 70%;
-}
+  }
 `;
 
-const Piece = ({ piece: {type, color} }) => {
 
-    const pieceImg = require(`./assets/${type}_${color}.png`)
+const Piece = ({ piece: { type, color } }) => {
+  const pieceIndex = `${type}_${color}`;
+  const pieceImg = require(`./assets/${pieceIndex}.png`);
+  const [{ isDragging }, drag, preview] = useDrag({
+    item: {
+      type: 'piece',
+      id: pieceIndex,
+    },
+    collect: (monitor) => {
+      return { isDragging: !!monitor.isDragging() }
+    },
+  })
 
-  return <PieceContainer><img src={pieceImg} alt="" /></PieceContainer>;
+  return (
+      <>
+    <DragPreviewImage connect={preview} src={pieceImg} />
+      <PieceContainer isDragging={isDragging} ref={drag}>
+        <img src={pieceImg} alt=""  />
+      </PieceContainer>
+</>
+
+  );
 };
 
 export default Piece;
